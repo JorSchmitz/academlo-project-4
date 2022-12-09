72dd4a3845a21bb4ed61fcb700e8255ca107b904
Jorge Piñeres <68415175+JorSchmitz@users.noreply.github.com>
2022-12-08 20:13:50 -0500

    Update App.jsx
    Changed base url

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import FormUsers from './components/FormUsers'
 import UserCard from './components/UserCard'
 
-const baseURL = `https://users-crud1.herokuapp.com/`
+const baseURL = `https://users-crud.academlo.tech/`
 
 function App() {
 
